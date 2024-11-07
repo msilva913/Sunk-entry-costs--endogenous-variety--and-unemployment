23854b4aea0eda05d82fd327761a0d5bf117105b
Mario Rafael Silva <msilva913@gmail.com>
2024-11-07 11:12:24 +0800

    Update solution

--- Programs baseline/run_solution.jl
+++ Programs baseline/run_solution.jl
@@ -6,13 +6,11 @@
 # These codes are free and without any guarantee. Use them at your own risk.
 # If you find typos, please let us know.
 
-#v1.7+
+cd(@__DIR__)
 using MKL
 using PyPlot
 using DataFrames
-#v1.7- 
-#BLAS.vendor() 
-#:mkl
+
 
 include("solution_functions.jl")
 include("steady_state.jl")
@@ -25,8 +23,8 @@ include("steady_state.jl")
         flag_SSsolver   = false
 
     # Parameters
-        @syms f_e δ s zbar b ϕ ρ σ ε A η_L F κ ξ_inv ρ_z  σ_z
-        parameters      = [f_e; δ; s; zbar; b; ϕ; ρ; σ; ε; A; η_L; F; κ; ξ_inv; ρ_z;  σ_z]
+        @syms f_e s zbar δbar b ϕ ρ σ ε A η_L F κ ξ_inv ρ_z σ_z ρ_δ σ_δ
+        parameters      = [f_e; s; zbar; δbar; b; ϕ; ρ; σ; ε; A; η_L; F; κ; ξ_inv; ρ_z; σ_z; ρ_δ; σ_δ]
         estimate        = []
         position        = []
         priors          = (;)
@@ -34,17 +32,17 @@ include("steady_state.jl")
         # Transformations
         β = 1/(1+ρ)
         ξ = 1/ξ_inv
-        τ = 1 - (1-δ)*(1-s)
+        τbar = 1 - (1-δbar)*(1-s)
         μ = ε/(ε-1)
 
     # Variables
-        @syms  z u θ q K L u v v_pret e K Q  N p N_e ν_f d_f w_R w L_e L_c Y_c C λ Y 
-        @syms zp θp qp Kp Lp up vp v_pretp ep Kp Qp Np pp N_ep ν_fp d_fp w_Rp wp L_ep L_cp Y_cp Cp λp Yp 
+        @syms  z δ u θ q K L u v v_pret e K Q  N p N_e ν_f d_f w_R w L_e L_c Y_c C λ Y 
+        @syms zp  δp θp qp Kp Lp up vp v_pretp ep Kp Qp Np pp N_ep ν_fp d_fp w_Rp wp L_ep L_cp Y_cp Cp λp Yp 
        
         
-        x               = [u; N; v_pret; z] # predetermined
+        x               = [u; N; v_pret; z; δ] # predetermined
         y               = [θ; q; L; v; e; K; Q; p; N_e; ν_f; d_f; w_R; w; L_e; L_c; Y_c; C; λ; Y]
-        xp              = [up; Np; v_pretp; zp]
+        xp              = [up; Np; v_pretp; zp; δp]
         yp              = [θp; qp; Lp; vp; ep; Kp; Qp; pp; N_ep; ν_fp; d_fp; w_Rp; wp; L_ep; L_cp; Y_cp; Cp; λp; Yp]
         variables       = [x; y; xp; yp]
         varnames = vcat(Symbol.(x), Symbol.(y))
@@ -52,21 +50,21 @@ include("steady_state.jl")
     # Shock
         @syms epsilon
         ex               = [epsilon]
-        eta = Array([0.0; 0.0; 0.0; -σ_z]) # size of state space
+        eta = Array([0.0; 0.0; 0.0; -σ_z; σ_δ]) # size of state space
 
     # Array of symbolics storing model equtions 
-    f = fill(Sym("x"), 23)
+    f = fill(Sym("x"), 24)
     # Equilibrium conditions
         # Job creation condition -> θ
-        f[1]  =  κ + K/q - β*λp/λ*(1-δ)*(w_Rp-wp-Kp+(1-s)*(κ+Kp/qp))
+        f[1]  =  κ + K/q - β*λp/λ*(1-δbar*δ)*(w_Rp-wp-Kp+(1-s)*(κ+Kp/qp))
         # Marginal revenue product -> w_R
         f[2] = w_R - p*z*zbar/μ
         # Wage equation -> w
-        f[3] = w - (ϕ*(w_R-K+θ/(1-δ)*(K+q*κ)) +(1-ϕ)*b)
+        f[3] = w - (ϕ*(w_R-K+θ/(1-δbar*δ)*(K+q*κ)) +(1-ϕ)*b)
         # Value of a vacancy -> Q
         f[4] = Q - (e/F)^(ξ_inv)
         # Expected discounted difference in vacancy value -> K
-        f[5] = K - (Q-β*λp/λ*(1-δ)*Qp)
+        f[5] = K - (Q-β*λp/λ*(1-δbar*δ)*Qp)
         # Market tightness -> v
         f[6] = θ - v/u 
         # Vacancy filling probability -> q
@@ -80,7 +78,7 @@ include("steady_state.jl")
         # Lagrangian multiplier -> λ
         f[11] = λ - C^(-σ)
         # Firm value -> ν_f
-        f[12] = ν_f - β*(1-δ)*λp/λ*(ν_fp+d_fp)
+        f[12] = ν_f - β*(1-δbar*δ)*λp/λ*(ν_fp+d_fp)
         # Retail output: resources -> Y_c
         f[13] = Y_c - p*z*zbar*L_c
         # Retail output: expenditure -> C
@@ -96,16 +94,17 @@ include("steady_state.jl")
         # LOM of vacancies 
         f[19] = v - (v_pret + e)
         # Predetermined vacancies 
-        f[20] = v_pretp - (1-δ)*((1-q)*v+s*(1-u))
+        f[20] = v_pretp - (1-δbar*δ)*((1-q)*v+s*(1-u))
         # LOM of unemployment
-        f[21] = up - ((1-(1-δ)*(θ*q))*u + τ*(1-u))
+        f[21] = up - ((1-(1-δbar*δ)*(θ*q))*u + (1-(1-δ*δbar)*(1-s))*(1-u))
         # LOM of firms 
-        f[22] = Np - (1-δ)*(N+N_e)
+        f[22] = Np - (1-δbar*δ)*(N+N_e)
         # Profits 
         #f[23] = d_f - Y_c/(N*ε)
 
         # Exogenous processes
         f[23]  =   log(zp) - ρ_z * log(z)
+        f[24] =    log(δp) -  ρ_δ * log(δ)
 
     # Steady state     
         # Values 
@@ -114,40 +113,42 @@ include("steady_state.jl")
         N_s = 1.0
         w_s = 1.0
         z_s = 1.0
+        δ_s = 1.0
+
         fbar = 0.41
         qbar = 0.8
         x_v = 0.20 
         labor_share = 0.66
 
         p_s = N_s^(1/(ε-1))
-        N_es = δ/(1-δ)*N_s
-        q_s = qbar/(1-δ)
+        N_es = δbar/(1-δbar)*N_s
+        q_s = qbar/(1-δbar)
         θ_s = fbar/qbar
-        u_s = τ/(τ+(1-δ)*(θ_s*q_s))
+        u_s = τbar/(τbar+(1-δbar)*(θ_s*q_s))
         L_s = 1 - u_s 
         v_s = θ_s*u_s
-        e_s = δ*(v_s+1-u_s)
+        e_s = δbar*(v_s+1-u_s)
         v_prets = v_s - e_s 
-        recruiter_share= (δ+(ρ+δ)*(ε-1))/(δ+(ρ+δ)*ε)
+        recruiter_share= (δbar+(ρ+δbar)*(ε-1))/(δbar+(ρ+δbar)*ε)
         w_wR = labor_share/recruiter_share
         w_Rs = w_s/(w_wR)
-        surplus_ratio = (ρ+τ)/(1-δ)*(1/(q_s*x_v))
+        surplus_ratio = (ρ+τbar)/(1-δbar)*(1/(q_s*x_v))
         K_s = (w_Rs-w_s)/(1+surplus_ratio) 
         κ   = (1-x_v)/x_v*K_s/q_s
-        f_e = (μ-1)*zbar*(L_s/N_s)*(1-δ)/(δ*μ+ρ)
+        f_e = (μ-1)*zbar*(L_s/N_s)*(1-δbar)/(δbar*μ+ρ)
         ν_fs =p_s*f_e/μ
-        d_fs = (ρ+δ)/(1-δ)*ν_fs
+        d_fs = (ρ+δbar)/(1-δbar)*ν_fs
         L_es = N_es*f_e/zbar
         L_cs = L_s - L_es 
         Y_cs = p_s*zbar*L_cs
-        Q_s = K_s*(1+ρ)/(ρ+δ)
+        Q_s = K_s*(1+ρ)/(ρ+δbar)
         C_s = Y_cs -  F/(1+ξ_inv)*(e_s/F)^(1+ξ_inv) -  κ*v_s*q_s 
         λ_s = C_s^(-σ)
         Y_s = Y_cs + ν_fs*N_es
         #x               = [u; N; v_pret; z] # predetermined
         #y               = [θ; q; L; v; e; K; Q; p; N_e; ν_f; d_f; w_R; w; L_e; L_c; Y_c; C; λ; Y]
         # Vector
-        SS_block  = [log(x) for x in [u_s, N_s, v_prets, z_s, θ_s, q_s, L_s, v_s, e_s, K_s, Q_s, p_s, N_es, ν_fs, d_fs, w_Rs, w_s, L_es, L_cs, Y_cs, C_s, λ_s, Y_s]]
+        SS_block  = [log(x) for x in [u_s, N_s, v_prets, z_s, δ_s, θ_s, q_s, L_s, v_s, e_s, K_s, Q_s, p_s, N_es, ν_fs, d_fs, w_Rs, w_s, L_es, L_cs, Y_cs, C_s, λ_s, Y_s]]
         # vertical concatenate: represent both current and future variables
         SS = vcat(SS_block, SS_block)
 
@@ -155,17 +156,22 @@ include("steady_state.jl")
         PAR_SS = parameters[:]
 
 
+    nx = length(x) 
+    ny = length(y)
+    nvar = nx + ny 
+    ne = length(ex)
+    nf = length(f) 
 
     # Procesing the model (no adjustment needed)                
         model = (parameters = parameters, estimate = estimate, estimation = position,
                 npar = length(parameters), ns = length(estimate), 
                 priors = priors,
                 x = x, y = y, xp = xp, yp = yp, variables = variables,
-                nx = length(x), ny = length(y), nvar = length(x) + length(y),
+                nx = nx, ny = ny, nvar = nvar,
                 e = ex, eta = eta,
-                ne = length(ex),
+                ne = ne,
                 f = f,
-                nf = length(f),
+                nf = nf,
                 SS = SS, PAR_SS = PAR_SS,
                 flag_order = flag_order, flag_deviation = flag_deviation, flag_SSsolver = flag_SSsolver)
         process_model(model)
@@ -182,11 +188,14 @@ include("steady_state.jl")
     # Shock values
     ρ_z = 0.975
     σ_z = 0.007
+    ρ_δ = 0.975
+    σ_δ = 0.044
 
     @unpack  f_e, δ, s, z, b, ϕ, ρ, σ, ε, A, η_L, F, κ, ξ_inv = cal
-    zbar = z
 
-    PAR     =   [f_e; δ; s; zbar; b; ϕ; ρ; σ; ε; A; η_L; F; κ; ξ_inv; ρ_z; σ_z ]
+    zbar = z 
+    δbar = δ
+    PAR     =   [f_e; s; zbar; δbar; b; ϕ; ρ; σ; ε; A; η_L; F; κ; ξ_inv; ρ_z; σ_z; ρ_δ; σ_δ ]
 
 
     # Solution functions (no adjustment needed)
@@ -198,7 +207,7 @@ include("steady_state.jl")
         SS_max = maximum(abs.(SS_err))
         println("Residuals: $SS_max")
 
-        ss = NamedTuple(zip(varnames, exp.(SS[1:23])))
+        ss = NamedTuple(zip(varnames, exp.(SS[1:nvar])))
 
         # @btime sol_mat = solve_model(model, deriv, eta)
         sol_mat = solve_model(model, deriv, eta)
