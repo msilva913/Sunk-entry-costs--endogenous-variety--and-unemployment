999ddc32465f2ac78f6bbcbfdf3e5f847dc9e3f1
Mario Rafael Silva <msilva913@gmail.com>
2024-11-21 16:53:51 +0800

    Modify observables with HP-filtered data

--- observables.py
+++ observables.py
@@ -47,7 +47,7 @@ def construct_data(init, final, freq):
     """
 
     " GDP Deflator BEA code A191RD"
-    deflator =  fred.get_series('GDPDEF').resample(freq).mean()
+    #deflator =  fred.get_series('GDPDEF').resample(freq).mean()
     #Y = fred.get_series('GDPC1').resample(freq).mean().dropna() #real, quarterly
     
     C = fred.get_series('PCE').resample(freq).mean()
@@ -67,7 +67,7 @@ def construct_data(init, final, freq):
     
     " Price index for investment goods "
     #p_I = fred.get_series("A006RD3Q086SBEA").resample(freq).mean().dropna()
-    p_I = fred.get_series("INVDEF").resample(freq).mean().dropna()
+    #p_I = fred.get_series("INVDEF").resample(freq).mean().dropna()
    
     #Equivalent to FPI (BEA A007RC) and change in business inventories (BEA CBI)
     #https://apps.bea.gov/iTable/?reqid=19&step=2&isuri=1&categories=underlying#eyJhcHBpZCI6MTksInN0ZXBzIjpbMSwyLDNdLCJkYXRhIjpbWyJjYXRlZ29yaWVzIiwiU3VydmV5Il0sWyJOSVBBX1RhYmxlX0xpc3QiLCIyMDcxIl1dfQ==
@@ -105,19 +105,6 @@ def construct_data(init, final, freq):
     
     
     " Vacancies "
-    # Vacancies
-    # df = pd.read_csv("https://raw.githubusercontent.com/letsgoexploring/economic-data/master/"
-    #                   "dmp/csv/beveridge_curve_data.csv")
-    # dates = df.iloc[:,0]
-    # df.index = pd.DatetimeIndex(dates)
-    # #u_bc = df.iloc[:,1]
-    # u_level = df.iloc[:, 1]
-    # lf = df.iloc[:,2]
-    # v = df.iloc[:,3]
-    # #u_bc = u_bc/lf
-    # # Create vacancy rate series by dividing vacancies by labor force
-    # v = v/lf
-    # v = v.resample(freq).mean().dropna()
     
     df = pd.read_csv("CompositeHWI.csv") # vacancy data based on Help-Wanted index
     date = pd.date_range(start="1951", end="2021-08", freq="MS")
@@ -150,7 +137,7 @@ def construct_data(init, final, freq):
     s = u_new/(e_level*(1-(1/2)*jf)) #separation rate series
     
     s = s.resample(freq).mean().dropna()
-    #f = jf.resample(freq).mean().dropna()
+    f = jf.resample(freq).mean().dropna()
     
     " Wages "
     # Nonfarm Business Sector: Real Hourly Compensation for All Workers, index 2017=100
@@ -159,14 +146,14 @@ def construct_data(init, final, freq):
 
     " Note: these series imply labor productivity in each sector "
     " List of data series "
-    var_load_list = [c, cons_share, u, v, theta, jf, lp, s, w] 
+    var_load_list = [c, cons_share, u, v, theta, f, lp, s, w] 
     return var_load_list
         
 if __name__ == "__main__":       
         # Baseline
     init= '1951-01-01'
     #final = '2024-05-30'
-    final='2020-02-01' # Just before pandemic shock
+    final='2020-01-01' # Just before pandemic shock
     # Comparison to earlier BRS
     #init = '1967-01-01'
     load = False
@@ -183,60 +170,73 @@ if __name__ == "__main__":
     dat = pd.concat(var_load_list, axis=1)
     lab = ['c', 'cons_share', 'u', 'v', 'theta', 'jf', 'lp', 's', 'w']
     dat.columns = lab
-    dat = dat.loc[init:final]
+    dat = dat.loc[init:final].dropna()
     
     " Data series in growth rates "
-    cycle_growth = pd.concat([filter_transform(dat[x], init=init, final=final, transform_type='log',
-                                        filter_type="growth", demean=False) for x in lab], axis=1)
-    cycle_growth.columns = lab
-    print(cycle_growth.mean())
-    cycle_growth = cycle_growth - cycle_growth.mean()
+    #cycle_growth = pd.concat([filter_transform(dat[x], init=init, final=final, transform_type='log',
+                                      #  filter_type="growth", demean=False) for x in lab], axis=1)
+    #cycle_growth.columns = lab
+    #print(cycle_growth.mean())
+    #cycle_growth = cycle_growth - cycle_growth.mean()
+    cycle_hp = pd.concat([filter_transform(dat[x], init=init, final=final, transform_type='log',
+                                        filter_type="hp_filter", lamb=10_000) for x in lab], axis=1)
+    cycle_hp.columns = lab
     
-    if save_observables:
-        " Save relevant objects "
-        #save_object(cycle, 'cycle')
-        " Save output for estimation using growth filter"
-        lab_obs = [x +'_obs' for x in lab]
-        dic_data = dict(zip(lab_obs, [np.asarray(cycle_growth[x]) for x in cycle_growth.columns]))
-        sio.savemat('observables.mat', dic_data)
+    mom = moments(cycle_hp, lab=["u"])
+    
+    # if save_observables:
+    #     " Save relevant objects "
+    #     #save_object(cycle, 'cycle')
+    #     " Save output for estimation using growth filter"
+    #     lab_obs = [x +'_obs' for x in lab]
+    #     dic_data = dict(zip(lab_obs, [np.asarray(cycle_growth[x]) for x in cycle_growth.columns]))
+    #     sio.savemat('observables.mat', dic_data)
     
     " Analysis on raw data "
-    
-    
-
-    "1) Beveridge curve "
-    fig, ax = plt.subplots()
-    ax.scatter(dat.u, dat.v, alpha=0.5)
-    ax.set_xlabel("Unemployment rate")
-    ax.set_ylabel("Vacancy rate")
-    plt.show()
-    dat[['u','v']].corr()
-    
-    "2) Unemployment, Vacancies, and ustar "
+    "1) Unemployment, Vacancies, and ustar "
     ustar = np.sqrt(dat.u*dat.v) # efficient unemployment rate (FERU) using approxiximation by Michaillat and Saez (2024)
     fig, ax = plt.subplots(figsize=(10, 4))
     ax.plot(dat.u["1951":], alpha=0.7, label="Unemployment rate")
     ax.plot(dat.v["1951":], alpha=0.7, label="Vacancy rate")
-    ax.plot(ustar["1951":], alpha=0.7, label="FERU")
+    #ax.plot(ustar["1951":], alpha=0.7, label="FERU")
     ax.xaxis.set_major_locator(years)
     ax.xaxis.set_major_formatter(years_fmt)
     ax.legend()
     plt.savefig("u_vac_series")
     plt.show()
     
+
+    "2) Beveridge curve (levels and filtered) "
+    fig, ax = plt.subplots(ncols=2, figsize=(12, 4))
+    ax[0].scatter(dat.u, dat.v, alpha=0.7)
+    ax[0].set_xlabel("Unemployment rate")
+    ax[0].set_ylabel("Vacancy rate")
+    ax[0].set_title("Levels")
+    ax[1].scatter(cycle_hp.u, cycle_hp.v, alpha=0.7)
+    ax[1].set_xlabel("Unemployment rate")
+    ax[1].set_ylabel("Vacancy rate")
+    ax[1].set_title("HP-filtered")
+    plt.show()
+    dat[['u','v']].corr()
+    
+
+    
     "3) Estimate elasticity of Beveridge curve "
-    log_u = np.log(dat.u)
-    log_v = np.log(dat.v)
+    #log_u = np.log(dat.u)
+    #log_v = np.log(dat.v)
+    u = cycle_hp.u
+    v = cycle_hp.v
     #X = log_u
-    X = sm.add_constant(log_u)
-    Y = log_v 
+    X = sm.add_constant(u)
+    Y = v
     #Newey west standard errors to control for heteroskedasticity and autocorrelation
-    model = sm.OLS(Y,X).fit(cov_type='HAC', cov_kwds={'maxlags':None})
+    #model = sm.OLS(Y,X).fit(cov_type='HAC', cov_kwds={'maxlags':None})
+    model = sm.OLS(Y,X).fit()
     print(model.summary())
     # Plot
     plt.figure(figsize=(10, 6))
-    plt.scatter(log_u, Y, alpha=0.5)
-    plt.plot(log_u, model.predict(X), color='red', linewidth=2, alpha=0.7, label=f"Slope={model.params.u:,.2f}")
+    plt.scatter(u, Y, alpha=0.5)
+    plt.plot(u, model.predict(X), color='red', linewidth=2, alpha=0.7, label=f"Slope={model.params.u:,.2f}")
     plt.xlabel('Log unemployment rate')
     plt.ylabel('Log vacancy rate')
     plt.title('Beveridge curve with regression line')
@@ -248,14 +248,16 @@ if __name__ == "__main__":
     # Impose m = Au^alpha v^(1-alpha)
     # Implies f = Atheta^(1-alpha)
     # In logs: log f = log A + (1-alpha)log theta
-    log_theta = np.log(dat.v/dat.u)
-    log_f = np.log(dat.f)
-    X = sm.add_constant(log_theta)
-    Y = log_f
+    #log_theta = np.log(dat.v/dat.u)
+    #log_f = np.log(dat.f)
+    #X = sm.add_constant(log_theta)
+    #Y = log_f
+    X = sm.add_constant(cycle_hp.theta)
+    Y = cycle_hp.jf
     model = sm.OLS(Y,X).fit(cov_type='HAC', cov_kwds={'maxlags':None})
     print(model.summary())
-    alpha_hat = 1-model.params[0]
-    A = np.exp(model.params.const)
+    alpha_hat = 1-model.params.theta
+    #A = np.exp(model.params.const)
     
     # Test for heteroskedasticity and autocorrelation (shifts of Beveridge curve)
     resid = model.resid
@@ -277,12 +279,7 @@ if __name__ == "__main__":
     
     
     
-    "8) Unemployment and labor productivity growth "
-    lab_prod_growth = np.log(dat.lab_prod).diff()
-    # Average annual growth
-    print(lab_prod_growth.mean()*4)
-    # Correlation
-    print(lab_prod_growth.corr(dat.u))
+    "4) Unemployment and labor productivity "
     
     # fig = plt.figure(figsize=(12, 10))
     # ax1 = fig.add_subplot(2, 1,1)
@@ -292,187 +289,55 @@ if __name__ == "__main__":
     # ax1.xaxis.set_major_formatter(years_fmt)
     # ax1.legend()
     
-    fig = plt.figure(figsize=(12, 8))
-    ax1 = fig.add_subplot(2,1,1)
-    ax1.scatter(dat.u, lab_prod_growth, alpha=0.5)
-    ax1.set_xlabel("Unemployment rate")
-    ax1.set_ylabel("Labor productivity growth")
-    ax1.legend()
-    
-    ax2 = fig.add_subplot(2,1,2)
-    ax2.scatter(np.log(dat.u).diff(), lab_prod_growth, alpha=0.5)
-    ax2.set_xlabel("Unemployment rate growth")
-    ax2.set_ylabel("Labor productivity growth")
-    ax2.legend()
-    plt.savefig("unemployment_labor_productivity.pdf")
-    plt.show()
+    fig, ax = plt.subplots(figsize=(8, 4))
+    #ax1 = fig.add_subplot(2,1,1)
+    ax.scatter(cycle_hp.u, cycle_hp.lp, alpha=0.5)
+    ax.set_xlabel("Unemployment rate")
+    ax.set_ylabel("Labor productivity")
+    ax.legend()
     
     
-    
-    "9) Dynamic correlation between unemployment and labor productivity growth"
-    ylabel=r'$Corr(u_{,t}, lab_prod_{t+\Delta})$'
-    labels = ["Unemployment rate", "Labor productivity growth"]
-    
-    
-    dat_red = pd.concat([dat.u, np.log(dat.u).diff(), dat.v, lab_prod_growth], axis=1)
-    index = ["u", "u_growth", "v", "lab_prod_growth"]
-    
-    dat_red.columns = index
-    var = ["u", "lab_prod_growth"]
-    dynamic_correlations(dat_red, var, ylabel, title="Dynamic correlations")
-    
-    var = ["u_growth", "lab_prod_growth"]
-    dynamic_correlations(dat_red, var, ylabel, title="Dynamic correlations")
-    
-    rolling_corr = dat_red["u"].rolling(window=30).corr(dat_red.lab_prod_growth)
-    plt.plot(rolling_corr)
-    rolling_corr = dat_red["u_growth"].rolling(window=30).corr(dat_red.lab_prod_growth)
-    
-    var = ["v", "lab_prod_growth"]
-    dynamic_correlations(dat_red, var, ylabel, title="Dynamic correlations")
-    
-    corr_mat = dat_idle[["idle_D", "idle_ND", "u_D", "u_ND"]].corr()
-    print(corr_mat)
     """
     Stylized facts
     1) Sectoral comovement (idleness, unemployment)
     2) Within-sector comovement of unemployment and idleness
     """
     
-    "7) Separation and job finding rates "
+    "5) Separation and job finding rates "
     # Not in current data: but separation rates spike dramatically in March 2023
-    fig = plt.figure(figsize=(12, 10))
-    ax1 = fig.add_subplot(2,1,1)
-    ax1.plot(dat.s, label="Separation rate")
-    ax2 = fig.add_subplot(2, 1, 2)
-    ax2.plot(dat.f, label="Job finding rate")
-    for ax in [ax1, ax2]:
-        ax.xaxis.set_major_locator(years)
-        ax.xaxis.set_major_formatter(years_fmt)
-        ax.legend()
+    fig, ax = plt.subplots(figsize=(10, 4))
+    ax.plot(cycle_hp.s, label="Separation rate", alpha=0.7, color="red", linestyle="--")
+    ax.plot(cycle_hp.jf, label="Job finding rate", alpha=0.7, color="blue")
+    ax.xaxis.set_major_locator(years)
+    ax.xaxis.set_major_formatter(years_fmt)
+    ax.legend()
     plt.show()
     
-    " Summary statistics"
+    " Summary statistics (in levels)"
     print("Mean consumption share:", dat.cons_share.mean())
     print("Mean Unemployment rate:", dat.u.mean())
     print("Mean vacancy rate:", dat.v.mean())
-    print("Mean annual labor productivity growth:", 4*lab_prod_growth.mean())
     print("Mean separation rate:", dat.s.mean())
-    print("Mean job finding rate:", dat.f.mean())
+    print("Mean job finding rate:", dat.jf.mean())
     print("Estimated matching function elasticity:", alpha_hat)
     
     
     
     # Comovement of unemployment, labor productivity, N
-    cycle_growth_labor = cycle_growth[['u', 'v','lab_prod', 'N']]
-    mom_growth_labor = moments(100*cycle_growth_labor, lab=['u','lab_prod'], lags=[1])
+    # cycle_growth_labor = cycle_growth[['u', 'v','lab_prod', 'N']]
+    # mom_growth_labor = moments(100*cycle_growth_labor, lab=['u','lab_prod'], lags=[1])
     
-    # Dependence on time period
-    corr_pre = cycle_growth_labor[init:"1983"].corr()
-    corr_post = cycle_growth_labor["1984":final].corr()
-    corrs_labor = pd.concat([corr_pre, corr_post])
+    # # Dependence on time period
+    # corr_pre = cycle_growth_labor[init:"1983"].corr()
+    # corr_post = cycle_growth_labor["1984":final].corr()
+    # corrs_labor = pd.concat([corr_pre, corr_post])
     
     # Sectoral comovement
-    cycle_growth_com = cycle_growth[['C', 'I', 'u_C', 'u_I', 'NC', 'NI','idle_D','idle_ND']]
-    mom_growth_com = moments(100*cycle_growth_com, lab=['I', 'NI'], lags=[1])
     
     
      
     
-    
-    " Summarize correlations"
-    corrs = np.zeros((8, 1))
-    corr_mat = cycle_growth.corr()
-    # Sectoral comovement
-    corrs[0] = corr_mat['C']['I']
-    corrs[1] = corr_mat['NC']['NI']
-    corrs[2] = corr_mat['u_C']['u_I']
-    corrs[3] = corr_mat['idle_ND']['idle_D']
-    corrs[4] = corr_mat['p_I']['I']
-    # Labor
-    corrs[5] = corr_mat['u']['N']
-    corrs[6] = corr_mat['u']['lab_prod']
-    corrs[7] = corr_mat['u']['v']
-    
-    cor_dat = pd.DataFrame(corrs)
-    cor_dat.index = ["Cor(C, I)", "Cor(NC, NI)", "Corr(u_C, u_I)",  "Cor(idle_ND, idle_D)", "Cor(p_I, I)",
-                      "Cor(u,N)" , "Corr(u, lab_prod)", "Cor(u,v)"]
-    
-    autocorr_dat = np.zeros((5, 1))
-    autocorr_dat[0] = cycle_growth.NC.autocorr()
-    autocorr_dat[1] = cycle_growth.NI.autocorr()
-    autocorr_dat[2] = cycle_growth.u.autocorr()
-    autocorr_dat[3] = cycle_growth.idle_ND.autocorr()
-    autocorr_dat[4] = cycle_growth.idle_D.autocorr()
-    autocorr_dat = pd.DataFrame(autocorr_dat)
-    autocorr_dat.index = ["Cor(N_C, N_{C,-1})", "Cor(N_I, N_{I,-1})", "Corr(u, u_{u,-1})",
-                          "Corr(idle_ND, idle_{ND,-1}", "Corr(idle_D, idle_{D,-1}"]
-    summ = pd.concat([cor_dat, autocorr_dat])
-    print(summ.style.format(precision=2).to_latex())
-    
-    # # Stacked moments
-    # stds = cycle_growth.std(axis=0)
-    # stds = 100*stds[["Y", "C", "I", "u_C", "u_I", "p_I", "idle_ND", "idle_D"]]
-    # stds = pd.DataFrame(stds)
-    # stds.index = ["std(Y)", "std(C)", "std(I)", "std(u_C)", "std(u_I)", "std(p_I)", 
-    #               "std(idle_ND)", "std(idle_D)"]
-    corrs = np.zeros((9, 1))
-    corr_mat = cycle_growth.corr()
-    # Outputs
-    corrs[0] = corr_mat['C']['I']
-    # Inputs
-    corrs[4] = corr_mat['u_C']['u_I']
-    corrs[5] = corr_mat['idle_ND']['idle_D']
-    # Cross relations
-    corrs[1] = corr_mat['C']['u_I']
-    corrs[2] = corr_mat['I']['u_C']
-    # u-v relation
-    corrs[2] = corr_mat['u']['lab_prod']
-              
-    # Relative price of investment
-    corrs[6] = corr_mat['p_I']['I']
-    cor_dat = pd.DataFrame(corrs)
-    cor_dat.index = ["Cor(C, I)", "Cor(C, N_I)",  "Cor(C, lab_prod)", "Cor(C, util)",
-                      "Cor(N_C, N_I)" , "Corr(util_D, util_ND)", "Cor(p_I, I)", "Cor(u, util_D)", "Cor(u, util_ND)"]
-    autocorr_dat = np.zeros((3, 1))
-    autocorr_dat[0] = cycle_growth.NC.autocorr()
-    autocorr_dat[1] = cycle_growth.NI.autocorr()
-    autocorr_dat[2] = cycle_growth.u.autocorr()
-    autocorr_dat = pd.DataFrame(autocorr_dat)
-    autocorr_dat.index = ["Cor(N_C, N_{C,-1})", "Cor(N_I, N_{I,-1})", "Corr(u, u_{u,-1})"]
-    summ = pd.concat([cor_dat, autocorr_dat])
-    print(summ.style.format(precision=2).to_latex())
-    
-    " For description: Hamilton-filtered series "
-    cycle_ham = pd.concat([filter_transform(dat[x], init=init, final=final, transform_type='log',
-                                        filter_type="ham", demean=False) for x in lab], axis=1)
-    cycle_ham.columns = lab
-    print(cycle_ham.mean())
-    cycle_growth = cycle_growth - cycle_growth.mean()
-    
-    
-    
-    
-    # Utilization
-    # fig, ax = plt.subplots(figsize=(11, 4))
-    # ax.plot(100*cycle.util_D, linestyle[0], label= "Utilization: durables", lw=2, alpha=0.7)
-    # ax.plot(100*cycle.util_ND, linestyle[1], label= "Utilization: nondurables", lw=2, alpha=0.7)
-    # ax.plot(100*cycle.u, linestyle[2], label="Unemployment rate", lw=2, alpha=0.7)
-    # #ax.plot(100*cycle.util, linestyle[2], label= "Utilization: total industry", lw=2, alpha=0.7)
-    # ax.legend(loc="upper right")
-    # ax.xaxis.set_major_locator(years)
-    # ax.xaxis.set_major_formatter(years_fmt)
-    # ax.set_ylabel("%")
-    # ax.grid(True)
-    # plt.tight_layout()
-    # plt.savefig("util_u_comovement.pdf")
-    # plt.show()
-    
-    # In levels 
-    # 1) Plot
-    
-    
-        
+   
     
         
         
