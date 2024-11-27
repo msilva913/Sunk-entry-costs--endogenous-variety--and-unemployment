1333bcc89fb1604b1a5bcc57f43f080fabc15248
Mario Rafael Silva <msilva913@gmail.com>
2024-11-27 13:06:14 +0800

    Update plots interface

--- Programs baseline/impulse_response_plots.jl
+++ Programs baseline/impulse_response_plots.jl
@@ -1,4 +1,6 @@
 using Plots
+import Plots:default
+default(linewidth=2, grid=true, fontfamily="Computer Modern")
 
 function gen_irf(irf_df::DataFrame)
     # Set up a 3x2 layout with specified size
