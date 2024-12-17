6fc5eded76964bc9242c52da900761524ab6a2da
Leslie Wu <leslieywu@gmail.com>
2024-12-17 18:25:21 +0800

    X-13

--- BFS.py
+++ BFS.py
@@ -4,14 +4,16 @@ from IPython.display import display
 from fredapi import Fred
 import numpy as np
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from statsmodels.tsa.x13 import x13_arima_analysis
 fred = Fred(api_key = 'd35aabd7dc07cd94481af3d1e2f0ecf3	')
 #df1 = pd.read_excel('SBF4.xlsx')
 #df2 = pd.read_excel('SBF8.xlsx')
 
 #define episodes
 episodes = {
-    'Great Recession': ('2007-12-01', '2009-07-01'),
-    'Pandemic Covid': ('2020-03-01', '2022-02-01')
+    'Great Recession': ('2007-01-01', '2009-12-31'),
+    'Pandemic Covid': ('2020-01-01', '2023-12-31')
 }
 
 #df_melted1 = df1.melt(id_vars='Year', var_name='Month', value_name='Value')
@@ -19,26 +21,57 @@ episodes = {
 
 df1 = fred.get_series('BFBF4QTOTALSAUS').resample('MS').mean().dropna()
 df2 = fred.get_series("BFBF8QTOTALSAUS").resample("MS").mean().dropna()
-bawba = fred.get_series("BAWBATOTALSAUS").resample("MS").mean().dropna()
+df3 = fred.get_series("BFDUR4QTOTALNSAUS").resample("MS").mean().dropna()
+
+x13_path = r"E:\文档\Github\Sunk-entry-costs--endogenous-variety--and-unemployment\x13as"
+result = x13_arima_analysis(df3, freq='M', x12path=x13_path, outlier=True, print_stdout=True)
+df3_adj= result.seasadj  # Seasonally adjusted series from X-13
+
+plt.figure(figsize=(12, 6))
+plt.plot(df3, label="Original Series", alpha=0.7)
+plt.plot(df3_adj, label="X-13 Adjusted Series", color="red", alpha=0.7)
+plt.legend()
+plt.title("DU4Q")
+plt.show()
+
 pop = fred.get_series('CNP16OV').resample('MS').mean().dropna()
 # Use HP-filtered trend for population to avoid discrete jumps around census dates
 pop = sm.tsa.filters.hpfilter(pop, lamb=10_000)[1]
-BF4 = df1/pop
-BF8 = df2/pop
-bawba = bawba/pop
-df = pd.concat([BF4, BF8, bawba], axis=1)
-df.columns = ["BF4", "BF8", "bawba"]
-df.dropna(inplace=True)
+df1 = df1/pop
+df2 = df2/pop
+df3 = df3_adj
+
+df1.dropna(inplace=True)
+df2.dropna(inplace=True)
+df3.dropna(inplace=True)
 # Divide business formation by population
 
-#plot
-df = np.log(df)
-df = df - df.iloc[0, :]
+#mapping
+#month_map = {
+#    'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6,
+#    'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12
+#}
 
+#define df
+# df_melted1['Month'] = df_melted1['Month'].map(month_map)
+# df_melted2['Month'] = df_melted2['Month'].map(month_map)
+
+# df_melted1['Year-Month'] = pd.to_datetime(df_melted1['Year'].astype(str) + '-' + df_melted1['Month'].astype(str))
+# df_melted2['Year-Month'] = pd.to_datetime(df_melted2['Year'].astype(str) + '-' + df_melted2['Month'].astype(str))
+
+# df_melted1.sort_values('Year-Month', inplace=True)
+# df_melted2.sort_values('Year-Month', inplace=True)
+
+# df_melted1.set_index('Year-Month', inplace=True)
+# df_melted2.set_index('Year-Month', inplace=True)
+
+# df_melted1.drop(['Year', 'Month'], axis=1, inplace=True)
+# df_melted2.drop(['Year', 'Month'], axis=1, inplace=True)
+
+#plot
 plt.figure(figsize=(12, 6))
-plt.plot(df.BF4, label='SBF4', color='blue')
-plt.plot(df.BF8, label='SBF8', color='red')
-plt.plot(df.bawba, label='Business applications: planned wages', color='magenta')
+plt.plot(df1.index, np.log(df1.values), label='SBF4', color='blue')
+plt.plot(df2.index, np.log(df2.values), label='SBF8', color='red')
 #plt.legend()
 
 for start, end in episodes.values():
@@ -46,11 +79,26 @@ for start, end in episodes.values():
 
 plt.title('Business Formation')
 plt.xlabel('Year')
-plt.ylabel(' Log Per Capita Business application and formation')
-plt.legend(loc='best')
+plt.ylabel(' Log Per Capita Business Formations')
+plt.legend(loc='upper left')
 plt.grid(True)
 plt.savefig('BFS_plot.pdf')
 plt.show()
+
+plt.plot(df3.index, df3.values, label='DUR4Q', color='green')
+
+for start, end in episodes.values():
+    plt.axvspan(start, end, color='gray', alpha=0.3)
+
+plt.title('Business Formation')
+plt.xlabel('Year')
+plt.ylabel('Number of Quarters')
+plt.ylim(0.4, 2.2)
+plt.legend(loc='upper left')
+plt.grid(True)
+plt.savefig('DUR4_plot.pdf')
+plt.show()
+
 
 #summary
 def create_summary_stats(df):
@@ -67,8 +115,11 @@ def create_summary_stats(df):
 
 sbf4_stats = create_summary_stats(df1)
 sbf8_stats = create_summary_stats(df2)
+dur4_stats = create_summary_stats(df3)
 
 print("SBF4 Summary Statistics:")
 display(sbf4_stats)
 print("\nSBF8 Summary Statistics:")
 display(sbf8_stats)
+print("\nDUR4 Summary Statistics:")
+display(dur4_stats)
---
+++ x13.py
@@ -0,0 +1,35 @@
+from statsmodels.tsa.x13 import x13_arima_analysis
+from statsmodels.tsa.seasonal import seasonal_decompose
+from fredapi import Fred
+import matplotlib.pyplot as plt
+
+
+fred = Fred(api_key='d35aabd7dc07cd94481af3d1e2f0ecf3')
+df3 = fred.get_series("BFDUR4QTOTALNSAUS").resample("MS").mean().dropna()
+
+# Path to X-13 (folder) executable
+x13_path = r"E:\文档\x13as"
+
+result1 = x13_arima_analysis(df3, freq='M', x12path=x13_path, outlier=True, print_stdout=True)
+result2 = seasonal_decompose(df3, model="additive", period=12)
+
+
+x13_seasonally_adjusted = result1.seasadj  # Seasonally adjusted series from X-13
+decompose_seasonally_adjusted = df3 - result2.seasonal  # Seasonally adjusted series from seasonal_decompose
+
+
+plt.figure(figsize=(14, 8))
+plt.plot(df3, label="Original Series", alpha=0.7)
+
+plt.plot(x13_seasonally_adjusted, label="X-13 Seasonally Adjusted", color="red", alpha=0.7)
+plt.plot(decompose_seasonally_adjusted, label="Seasonal Decompose Adjusted", color="green", alpha=0.7)
+
+plt.legend(loc="best", fontsize=12)
+plt.title("Comparison of Original, X-13, and Seasonal Decompose Adjusted Series", fontsize=16)
+plt.xlabel("Date", fontsize=12)
+plt.ylabel("DU4Q", fontsize=12)
+plt.grid(True)
+
+
+plt.tight_layout()
+plt.show()
