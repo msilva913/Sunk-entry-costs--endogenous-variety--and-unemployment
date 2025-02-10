bda19ec6271a5b7bd7bb7ff4b7ea5dd05ae9cb04
Mario Rafael Silva <msilva913@gmail.com>
2025-02-10 15:42:09 +0800

    Update steady_state_checks_general_CES.jl

--- Programs baseline/steady_state_checks_general_CES.jl
+++ Programs baseline/steady_state_checks_general_CES.jl
@@ -69,7 +69,6 @@ steady = steady_state(para)
 # targets = (ϕ=0.6, dest_ann=0.06, r_ann=0.04, f =0.41, η_L=0.6, q=0.8, sep=0.031, b_ratio=0.71, ξ_inv=1, ε=4, σ=1.5, N=1, w=1.0)
 # cal = calibrate(targets)
 # steady = steady_state(cal)
-
 # Examination of very high elasticity of substitution
 para = (cal...,  ε=1e12)
 steady = steady_state(para)
