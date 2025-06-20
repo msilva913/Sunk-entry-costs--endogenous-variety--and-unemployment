78172e3a49c324bb8c7d91c5847e2a5a39fd5383
Mario Rafael Silva <msilva913@gmail.com>
2025-06-20 12:56:12 +0800

    Update estimation_analysis and generate_tables

--- OurModel_Estimation_Dynare4.4.3/estimation_analysis.jl
+++ OurModel_Estimation_Dynare4.4.3/estimation_analysis.jl
@@ -12,7 +12,8 @@ using Dates, Distributions
 
 homedir()
 #include("time_series_fun.jl")
-cd(@__DIR__)
+#cd(@__DIR__)
+cd("C:/Users/msilva913/Documents/GitHub/Sunk_entry_costs_endogenous_variety_unemployment")
 columns(M) = (view(M, :, i) for i in 1:size(M, 2))
 
 posterior = matopen("posterior_density.mat")
@@ -21,9 +22,19 @@ struc = read(posterior, "posterior_density")
 print(keys(struc))
 n = length(keys(struc))
 
+# Priors 
+priors = matopen("priors.mat")
+priors = read(priors, "priors")
+priors_mean = priors["mean"]
+
 # Posterior mean
 posterior_mean = matopen("posterior_mean.mat")
 posterior_mean_struc = read(posterior_mean, "posterior_mean")
+
+# Posterior mode 
+posterior_mode = matopen("posterior_mode.mat")
+posterior_mode_struc = read(posterior_mode, "posterior_mode")
+
 
 # Posterior standard deviation
 # posterior_std = matopen("posterior_std.mat")
@@ -66,19 +77,19 @@ return alpha, beta
 end 
 
 
-
-# sigma,      gamma_pdf,      1.5, 0.5;
-# b_ratio,    beta_pdf,       0.71, 0.2;
-# x_v,        beta_pdf,       0.5, 0.25;
-# xi_inv,     gamma_pdf,      1.0, 2;
-# delta,      beta_pdf,       0.0083, 0.005; 0.0083 
-# epsi,       gamma_pdf,      4.2, 1.5;
-# rho_z,      beta_pdf,       0.8, 0.2;
-# rho_delta,  beta_pdf,       0.93, 0.1;
-# rho_s,      beta_pdf,       0.8, 0.2;
-# sigma_z, 0.01, 0.000001, 0.2,    inv_gamma_pdf,  0.01, 1.0;
-# sigma_delta, 0.01, 0.00001, 0.2,     inv_gamma_pdf,  0.02, 0.01; 
-# sigma_s, 0.01, 0.00001, 0.2,            inv_gamma_pdf,  0.01, 1.0;
+"""
+sigma,      gamma_pdf,      1.5, 0.5;
+b_ratio,    beta_pdf,       0.7, 0.1;
+xi_inv,     gamma_pdf,      2.0, 2.0;
+x_v,        beta_pdf,       0.5, 0.2;
+epsi,       gamma_pdf,      4.2, 1.5;
+rho_z,      beta_pdf,       0.8, 0.10;
+rho_s,      beta_pdf,       0.6, 0.20;
+rho_delta,  beta_pdf,       0.9, 0.05;
+sigma_z,    inv_gamma_pdf,  0.01, 0.01;
+sigma_s,    inv_gamma_pdf,  0.06, 0.06;
+sigma_delta,inv_gamma_pdf,  0.02, 0.02;
+"""
 
 # sigma prior 
 α, β = gamma_map(1.5, 0.5)
@@ -87,25 +98,26 @@ sigma_x = 0.5:0.1:2.0
 sigma_prior_pdf = pdf(gamma_dist, sigma_x)
 
 # xi_inv prior 
-α, β = gamma_map(1.0, 2.0)
+α, β = gamma_map(2.0, 2.0)
 gamma_dist = Gamma(α, β)
-ξ_inv_x = 0:0.1:6.0
+ξ_inv_x = 0:0.05:1.0
 ξ_prior_pdf = pdf(gamma_dist, ξ_inv_x)
 
 # epsi prior
 α, β = gamma_map(4.2, 1.5)
 gamma_dist = Gamma(α, β)
-epsi_x = 2:0.1:6.0
+epsi_x = 2:0.1:10
 epsi_prior_pdf = pdf(gamma_dist, epsi_x)
 
 #δ prior
-α, β = beta_map(0.0083, 0.005)
-beta_dist = Beta(α, β)
-delta_x = 0.006:0.00025:0.025
-delta_prior_pdf = pdf(beta_dist, delta_x)
+
+# α, β = beta_map(0.0083, 0.005)
+# beta_dist = Beta(α, β)
+# delta_x = 0.006:0.00025:0.025
+# delta_prior_pdf = pdf(beta_dist, delta_x)
 
 #b prior 
-α, β = beta_map(0.71, 0.2)
+α, β = beta_map(0.7, 0.2)
 beta_dist = Beta(α, β)
 b_x = 0.6:0.005:0.96
 b_prior_pdf = pdf(beta_dist, b_x)
@@ -117,7 +129,7 @@ b_prior_pdf = pdf(beta_dist, b_x)
 sigma_vals, sigma_density = columns(struc["sigma"])
 ξ_inv_vals, ξ_inv_density = columns(struc["xi_inv"])
 epsi_vals, epsi_density = columns(struc["epsi"])
-delta_vals, delta_density = columns(struc["delta"])
+#delta_vals, delta_density = columns(struc["delta"])
 b_vals, b_density = columns(struc["b_ratio"])
 
 # Density of shock parameters
@@ -133,11 +145,12 @@ params = [
     Dict("vals" => sigma_vals, "density" => sigma_density, "x" => sigma_x, "prior_pdf"=> sigma_prior_pdf, "xlabel"=> "σ"),
     Dict("vals" => ξ_inv_vals, "density" => ξ_inv_density, "x" => ξ_inv_x, "prior_pdf" => ξ_prior_pdf, "xlabel" => "ξ_inv"),
     Dict("vals" => epsi_vals, "density" => epsi_density, "x" => epsi_x, "prior_pdf" => epsi_prior_pdf, "xlabel" => "ε"),
-    Dict("vals" => delta_vals, "density" => delta_density, "x" => delta_x, "prior_pdf" => delta_prior_pdf, "xlabel" => "δ"),
     Dict("vals" => b_vals, "density" => b_density, "x" => b_x, "prior_pdf" => b_prior_pdf, "xlabel" => "b")
+    #Dict("vals" => delta_vals, "density" => delta_density, "x" => delta_x, "prior_pdf" => delta_prior_pdf, "xlabel" => "δ"),
+   
 ]
 
-fig, axs = subplots(2, 3, figsize=(18, 6))
+fig, axs = subplots(2, 2, figsize=(10, 6))
 axs = axs[:]
 
 for i in 1:length(params)
@@ -154,10 +167,10 @@ for i in 1:length(params)
     ax.set_xlim(minimum(param["x"]), maximum(param["x"]))
     ax.set_ylim(0, maximum(param["density"]) * 1.1)
 end
-fig.delaxes(axs[end])
+#fig.delaxes(axs[end])
 tight_layout()
 display(fig)
-plt.savefig("posterior_prior_plots.pdf")
+plt.savefig("posterior_prior_plots_structural.pdf")
 
 
 # Shock processes
@@ -168,32 +181,33 @@ rho_z_x = 0.7:0.001:0.999
 rho_z_prior_pdf = pdf(beta_dist, rho_z_x)
 
 #rho_delta
-α, β = beta_map(0.93, 0.05)
+α, β = beta_map(0.9, 0.05)
 beta_dist = Beta(α, β)
 rho_delta_x = 0.8:0.01:0.99
 rho_delta_prior_pdf = pdf(beta_dist, rho_delta_x)
 
 # rho_s
-α, β = beta_map(0.8, 0.1)
+α, β = beta_map(0.6, 0.2)
 beta_dist = Beta(α, β)
 rho_s_x = 0.7:0.01:0.99
 rho_s_prior_pdf = pdf(beta_dist, rho_s_x)
 
 # sigma_z prior 
-α, β = inverse_gamma_map(0.01, 1.0)
+α, β = inverse_gamma_map(0.01, 0.01)
 inverse_gamma_dist = InverseGamma(α, β)
-sigma_z_x = 0.001:0.001:0.06
+sigma_z_x = 0.001:0.001:0.03
 sigma_z_prior_pdf = pdf(inverse_gamma_dist, sigma_z_x)
 
 # sigma_delta prior 
-α, β = inverse_gamma_map(0.02, 0.01)
+α, β = inverse_gamma_map(0.02, 0.02)
 inverse_gamma_dist = InverseGamma(α, β)
-sigma_delta_x = 0.001:0.005:0.06
+sigma_delta_x = 0.001:0.005:0.03
 sigma_delta_prior_pdf = pdf(inverse_gamma_dist, sigma_delta_x)
 
-α, β = inverse_gamma_map(0.01, 1.0)
+# sigma_s prior
+α, β = inverse_gamma_map(0.06, 0.06)
 inverse_gamma_dist = InverseGamma(α, β)
-sigma_s_x = 0.001:0.001:0.06
+sigma_s_x = 0.001:0.001:0.1
 sigma_s_prior_pdf = pdf(inverse_gamma_dist, sigma_s_x)
 
 
@@ -206,7 +220,7 @@ params = [
     Dict("vals" => sigma_s_vals, "density" => sigma_s_density, "x" => sigma_s_x, "prior_pdf" => sigma_s_prior_pdf, "xlabel" => "σ_s")
 ]
 
-fig, axs = subplots(2, 3, figsize=(14, 4))
+fig, axs = subplots(2, 3, figsize=(12, 5))
 axs = [axs[i, j] for j in 1:size(axs, 2), i in 1:size(axs, 1)]
 for (i, ax) in enumerate(axs)
     param = params[i]
@@ -224,10 +238,88 @@ end
 
 tight_layout()
 display(fig)
-savefig("posterior_priors_shocks.pdf")
+savefig("posterior_prior_plots_shocks.pdf")
+
+####### Posterior-prior tables ########
+posterior_HPDlow = matopen("posterior_HPDlow.mat")
+posterior_HPDlow = read(posterior_HPDlow, "posterior_HPDlow")
+
+posterior_HPDhigh = matopen("posterior_HPDhigh.mat")
+posterior_HPDhigh = read(posterior_HPDhigh, "posterior_HPDhigh")
+
+posterior_std = matopen("posterior_std.mat")
+posterior_std = read(posterior_std, "posterior_std")
 
 
+prior_dict = Dict(
+    "sigma"       => ("Gamma", 1.5, 0.5),
+    "b_ratio"     => ("Beta", 0.7, 0.1),
+    "xi_inv"      => ("Gamma", 2.0, 2.0),
+    "x_v"         => ("Beta", 0.5, 0.2),
+    "epsi"        => ("Gamma", 4.2, 1.5),
+    "rho_z"       => ("Beta", 0.8, 0.10),
+    "rho_s"       => ("Beta", 0.6, 0.20),
+    "rho_delta"   => ("Beta", 0.9, 0.05),
+    "sigma_z"     => ("Inverse Gamma", 0.01, 0.01),
+    "sigma_s"     => ("Inverse Gamma", 0.06, 0.06),
+    "sigma_delta" => ("Inverse Gamma", 0.02, 0.02)
+)
 
 
+print(keys(posterior_mean_struc))
+keys_r = ["sigma", "b_ratio", "xi_inv", "x_v", "epsi", "rho_z", "rho_s", "rho_delta", "sigma_z", "sigma_s", "sigma_delta"]
+
+posterior_table = DataFrame(
+    Parameter = keys_r,
+    PriorDistribution = [prior_dict[param][1] for param in keys_r],
+    PriorMean =  [prior_dict[param][2] for param in keys_r],
+    PriorStd = [prior_dict[param][3] for param in keys_r],
+    PosteriorMean = [posterior_mean_struc[param] for param in keys_r],
+    PosteriorStd = [posterior_std[param] for param in keys_r],
+    PosteriorMode = [posterior_mode_struc[param] for param in keys_r],
+    LowerHPD = [posterior_HPDlow[param] for param in keys_r],
+    HigherHPD =[posterior_HPDhigh[param] for param in keys_r]
+)
+
+for col in names(posterior_table)
+    if eltype(posterior_table[!, col]) <: AbstractFloat  # Assuming the first column is non-numeric (like 'Parameter')
+    posterior_table[!, col] = round.(posterior_table[!, col], sigdigits=3)
+    end
+end
+
+print(posterior_table)
 
 
+function dataframe_to_latex_multicolumn(df::DataFrame)
+    header = [
+        "\\begin{table}[ht]",
+        "\\centering",
+        "\\begin{tabular}{c|ccc|ccccc}",
+        "\\toprule",
+        "& \\multicolumn{3}{c|}{Priors} & \\multicolumn{5}{c|}{Posteriors} \\\\ \\midrule",
+        "Parameter & Distribution & Mean & Std & Mean & Mode & Std & Lower HPD & Higher HPD \\\\"
+    ]
+
+    rows = [
+        string(
+            row.Parameter, " & ", row.PriorDistribution, " & ",
+            round(row.PriorMean, sigdigits=2), " & ", round(row.PriorStd, sigdigits=2), " & ",
+            round(row.PosteriorMean, sigdigits=2), " & ", round(row.PosteriorMode, sigdigits=2), " & ",
+            round(row.PosteriorStd, sigdigits=2), " & ", round(row.LowerHPD, sigdigits=2), " & ", round(row.HigherHPD, sigdigits=2),
+            " \\\\"
+        ) for row in eachrow(df)
+    ]
+
+    footer = [
+        "\\bottomrule",
+        "\\end{tabular}",
+        "\\caption{Posterior estimates for model parameters with prior information.}",
+        "\\label{tab:posterior_estimates}",
+        "\\end{table}"
+    ]
+
+    return join([header; rows; footer], "\n")
+end
+
+latex_table = dataframe_to_latex_multicolumn(posterior_table)
+println(latex_table)
--- Programs baseline/generate_tables.jl
+++ Programs baseline/generate_tables.jl
@@ -1,4 +1,14 @@
+"""
+Generate tables for 
+    1) calibration 
+    2) steady-state shares
+"""
+
 include("steady_state.jl")
+cd("C:/Users/msilva913/Documents/GitHub/Sunk_entry_costs_endogenous_variety_unemployment")
+# Load posterior mode 
+posterior_mode = matopen("posterior_mode.mat")
+posterior_mode = read(posterior_mode, "posterior_mode")
 
 targets = (labor_share=0.66, 
            dest_ann=0.10, 
@@ -7,12 +17,13 @@ targets = (labor_share=0.66,
            η_L=0.6, #elast. of matching function
            q=0.8, 
            sep=0.031, 
-           b_ratio=0.71, 
-            x_v=0.20, 
-            ξ_inv=1, # congestion elasticity
-            ε=4.3, # elasticity of sub.
-            σ=1.0, # log utility
+           b_ratio=posterior_mode["b_ratio"], 
+            x_v=posterior_mode["x_v"], 
+            ξ_inv=posterior_mode["xi_inv"], # congestion elasticity
+            ε=posterior_mode["epsi"], # elasticity of sub.
+            σ=posterior_mode["sigma"], # log utility
             N=1, w=1.0)
+
 cal = calibrate_labor_share(targets)
 ss = steady_state(cal)
 df = calibration_table(cal, targets)
@@ -27,10 +38,10 @@ function shares_table(ss::NamedTuple)
     df = DataFrame(
         Share = [
             "Consumption share",
-            "Vacancy share",
+            "Recruiting cost share",
             "Investment in new product lines",
             "Sunk vacancy cost share",
-            "Entrant share",
+            "New vacancy share",
             "Search wedge",
             "Market power wedge",
             "Stock market cap to GDP"
@@ -44,7 +55,36 @@ end
 
 df_shares = shares_table(ss)
 
+"""
 output = IOBuffer()
 show(output, MIME("text/latex"),df_shares)
 df_shares_table = String(take!(output))
 print(df_shares_table)
+"""
+function dataframe_to_latex(df::DataFrame)
+    header = [
+        "\\begin{table}[ht]",
+        "\\centering",
+        "\\begin{tabular}{l|cc}",
+        "\\toprule",
+        "Share & Symbol & Value \\\\ \\midrule"
+    ]
+
+    rows = [
+        string(
+            row.Share, " & ", row.Symbol, " & ", round(row.Value, digits=3), " \\\\"
+        ) for row in eachrow(df)
+    ]
+
+    footer = [
+        "\\bottomrule",
+        "\\end{tabular}",
+        "\\caption{}",
+        "\\end{table}"
+    ]
+
+    return join([header; rows; footer], "\n")
+end
+
+latex_table = dataframe_to_latex(df_shares)
+println(latex_table)
