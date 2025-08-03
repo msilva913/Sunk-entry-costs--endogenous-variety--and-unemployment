29b1e63fd56c6e8290c66ee134d746b53ef2aee0
Mario Rafael Silva <38724157+msilva913@users.noreply.github.com>
2025-08-03 13:55:14 +0800

    Update steady_state_checks_CK.jl

--- Programs baseline/steady_state_checks_CK.jl
+++ Programs baseline/steady_state_checks_CK.jl
@@ -1,8 +1,9 @@
 include("steady_state_CK.jl")
 # Original calibration in Coles and Kelishomi
 targets = (ϕ=0.6, r_ann=0.04, f =0.41, η_L=0.6, q=0.8, sep=0.034, b_ratio=0.71, ξ_inv=1/0.265, w=1.0)
-targets = (ϕ=0.6, r_ann=0.04, f =0.41, η_L=0.6, q=0.8, sep=0.034, b_ratio=0.71, ξ_inv=1, w=1.0)
-cal = calibrate(targets)
+# CK calibration following z = 1 normalization
+targets = (ϕ=0.6, r_ann=0.04, f =0.41, η_L=0.6, q=0.8, sep=0.034, b_ratio=0.71, ξ_inv=1, z=1.0)
+cal = calibrate_alt(targets)
 
 steady = steady_state(cal)
 @unpack θ, w, L, K, q, C, Y, X, labor_share, M, e = steady
